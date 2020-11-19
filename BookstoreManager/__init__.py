c8979caf3d4b7a450cc968be8da073dda52de1f6
ABC <1851050185vin@ou.edu.vn>
2020-11-19 15:16:42 +0700

    complele templates

--- BookstoreManager/__init__.py
+++ BookstoreManager/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = "y1478whdbh1183132qwchda"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:P@ssw0rd@localhost/webstoredb?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Abc1234%^&@localhost/webstoredb?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app=app)
 admin = Admin(app=app, name='Quản trị hệ thống',
