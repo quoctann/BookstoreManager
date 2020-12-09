974f5fb6b309adcd507256796c115eb5c8433bfe
ABC <1851050185vin@ou.edu.vn>
2020-12-09 12:54:43 +0700

    . . .

--- BookstoreManager/__init__.py
+++ BookstoreManager/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mail import Mail
+from flask_mail import Mail  
 
 
 app = Flask(__name__)
