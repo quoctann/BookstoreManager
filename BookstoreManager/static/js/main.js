5d7f8b91ee3c217930ded7dbd60af5609da258bd
Quoc Tan <tqtwork@outlook.com>
2020-12-09 11:57:09 +0700

    Sell task beta v1

--- BookstoreManager/static/js/main.js
+++ BookstoreManager/static/js/main.js
@@ -1,10 +1,4 @@
-// Global variable that any function can be accessible
-var inputID = 0;
-var totalItem = 0;
-var dt = new Date();
-document.getElementById("invoice-date").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
-
-// Toggle displaying navbar
+// Ẩn hiện menu bên
 $(function() {
   // Sidebar toggle behavior
   $('#sidebarCollapse').on('click', function() {
@@ -13,20 +7,40 @@ $(function() {
 })
 
 
-// Reset everything to default value
+// BIẾN TOÀN CỤC
+// Khi ở trang sellview, giá trị này lưu thuộc tính name của ô input sản phẩm cuối cùng
+// có thể sử dụng trong vòng lặp để duyệt lấy request phía backend
+var inputID = 0;
+// Lưu trữ tổng số ô input sản phẩm ở sellview
+var totalItem = 0;
+// Gắn giá trị ngày tháng hiện tại vào document, DD/MM/YYY HH:MM
+var dt = new Date();
+document.getElementById("invoice-date").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+
+
+// Khởi tạo các giá trị toàn cục về mặc định
 function init() {
     inputID = 0;
     totalItem = 0;
 }
 
-// Add a HTML element
+// Thêm một trường mới để nhập mã sản phẩm, số lượng ở sellview
 function addProduct() {
   const div = document.createElement('div');
-  div.className = 'form-group d-flex';
+  div.className = 'form-inline d-flex my-2';
   div.innerHTML = `
-        <input type="text" class="form-control mr-2" name=` +
-        inputID +
-        ` placeholder="Mã sản phẩm">
+        <label for=
+        ` + inputID + `
+        >Mã sản phẩm: </label>
+        <input type="text" class="form-control mx-2" name=
+        ` + inputID + `
+        placeholder="Ví dụ: 1234">
+        <label for=
+        ` + ('quantity-' + inputID) + `
+        >Mã sản phẩm: </label>
+        <input type="number" class="form-control mx-2" name=
+        ` + ('quantity-' + inputID) + `
+        min=1 max=5 value=1 placeholder="Tối đa 5">
         <input type="button" class="btn btn-secondary" value="Xóa" onclick="removeRow(this)" />
   `;
   inputID++;
@@ -34,7 +48,7 @@ function addProduct() {
   document.getElementById('sell_form').appendChild(div);
 }
 
-// Remove a HTML element
+// Xóa phần tử input được tạo bên trên
 function removeRow(input) {
   totalItem--;
   document.getElementById('sell_form').removeChild(input.parentNode);
@@ -45,4 +59,26 @@ function printDate() {
     date = Date();
     document.getElementById("invoice-date").innerHTML = date;
     return date;
+}
+
+
+// Promises, have a catch()
+function myFunction(id) {
+    fetch('/api/get_value', {
+        method: "post",
+        body: JSON.stringify({
+            "id": id,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json()).then(data => {
+        console.info(data);
+        var sometime = document.getElementById("something");
+        sometime.innerText = `${data.message}`;
+    }).catch(err => {
+        console.log(err);
+    })
+
+    // promise --> await/async
 }
